fbd885da9b49293e82dae66e4c38d6a55c948b95
Manuela320 <manuelaandricioaei63@gmail.com>
2024-09-14 19:32:35 +0300

    hw 03

---
+++ src/components/Loader/Loader.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import { Circles } from 'react-loader-spinner';
+// import 'react-loader-spinner/dist/loader/css/react-spinner-loader.css';
+import styles from './Loader.module.css';
+
+const Loader = () => {
+  return (
+    <diiv>
+      <Circles color="#00BFFF" height={80} width={80} />
+    </diiv>
+  );
+};
+
+export default Loader;
